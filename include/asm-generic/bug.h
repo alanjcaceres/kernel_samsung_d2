cf5e39188320edee716c5e16f9cec1597a18a5af
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-03-22 08:33:07 +0100

    extern printf(x, y) --> extern __attribute__((format(printf, x, y)));
    Change-Id: Icc9e1a403cb916e984454125cd601ff9f86d8ca8
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>

--- include/asm-generic/bug.h
+++ include/asm-generic/bug.h
@@ -61,10 +61,10 @@ struct bug_entry {
  */
 #ifndef __WARN_TAINT
 #ifndef __ASSEMBLY__
-extern __printf(3, 4)
+extern __attribute__((format(printf, 3, 4)));
 void warn_slowpath_fmt(const char *file, const int line,
 		       const char *fmt, ...);
-extern __printf(4, 5)
+extern __attribute__((format(printf, 4, 5)));
 void warn_slowpath_fmt_taint(const char *file, const int line, unsigned taint,
 			     const char *fmt, ...);
 extern void warn_slowpath_null(const char *file, const int line);
